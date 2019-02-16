9c71e4a711193e506e781b8eab0b0b0ed5f172d6
Chris Eppstein <chris@eppsteins.net>
2019-02-16 14:21:12 -0800

    Remove files to avoid confusion.

--- examples/example-1/Brocfile.js
+++
@@ -1,9 +0,0 @@
-var BroccoliEyeglass = require("broccoli-eyeglass");
-
-var options = {
-  cssDir: "css" /* This is the only required option */,
-};
-
-var outputTree = new BroccoliEyeglass(["src"], options);
-
-module.exports = outputTree;
--- test/fixtures/manualModule/eyeglass-exports.js
+++
@@ -1,7 +0,0 @@
-var path = require("path");
-
-module.exports = function() {
-  return {
-    sassDir: path.join(__dirname, "sass"),
-  };
-};
